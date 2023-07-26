d0311f7fc6ca31194fa3de35f48b1ee304b76c98
Rinkesh Dhakad <140053663+royaldhakad7024@users.noreply.github.com>
2023-07-26 23:04:33 +0530

    Update project.py

--- project.py
+++ project.py
@@ -5,6 +5,9 @@ print("""option:
       2-paper
       3-scissors
       """)
+us=0
+cs=0
+tie=0
 while True:
     uc=int(input("""Game Start...
                  1 Yes
@@ -21,19 +24,41 @@ while True:
         elif userInput==2:
             uchoice="paper"
         elif userInput==3:
-            uchoice="scissors"
+            uchoice="scissors"   
         cchoice=random.choice(l)
         if uchoice==cchoice:
             print("Computer💻 Value=",cchoice)
             print("User👨‍🏭 Value=",uchoice)
             print("Game Draw🥳✨")
+            tie+=1
+            # print("tie score is=",tie)
+            # print("user score is=",us)
+            # print("computer score is=",cs)
         elif (uchoice=="rock" and cchoice=="scissors") or (uchoice=="paper" and cchoice=="rock") or (uchoice=="scissors" and cchoice=="paper"):
             print("Computer💻 Value=",cchoice)
             print("User👨‍🏭 Value=",uchoice)
             print("You Win🥳✨")
+            us+=1
+            # print("tie score is=",tie)
+            # print("user score is=",us)
+            # print("computer score is=",cs)
         else:
             print("Computer💻 Value=",cchoice)
             print("User👨‍🏭 Value=",uchoice)  
             print("Computer Win🥳✨")
+            cs+=1
+            # print("tie score is=",tie)
+            # print("user score is=",us)
+            # print("computer score is=",cs)
+
     else:
-        break          
+        break     
+print("tie score is=",tie)
+print("user score is=",us)
+print("computer score is=",cs)
+if us>cs:
+    print("final winner is user🥳")
+elif cs>us:
+    print("final winner is computer😱")  
+else:
+    print("tie👔")
